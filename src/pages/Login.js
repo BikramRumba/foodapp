370c65e6ea244400cec21341c67bb5107fb0825c
BikramRumba <rumbikram123@gmail.com>
2022-02-07 01:45:59 -0600

    added login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import {Form, Button, Card} from 'react-bootstrap';
+import strawberry from '../assets/seasoning.jpeg';
+import '../styles/Login.css';
 
 function Login() {
   return (
-  <div>
-      WElcome to Login page
+  <div className='register' style={{backgroundImage: `url(${strawberry})`}} >
+      <div className='middle'>
+       
+        <form id='signin-form' method ='POST'>
+          <h1>Sign In</h1>
+            <label htmlFor="email">Email Address</label>
+            <input type="email" required name='name' 
+            placeholder='Enter your email address' />
+
+            <label htmlFor="password" >Password</label>
+            <input type='password' required placeholder='Enter your password'/>
+              
+              <button>Sign In</button>
+        </form>
+        <div className="reset-password">
+          Forgot Password?
+        </div>
+        <div className="new-account">
+          Create a new account? Signup
+        </div>
+      </div>
   </div>
   );
 }
